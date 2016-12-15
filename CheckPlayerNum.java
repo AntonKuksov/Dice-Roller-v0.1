a890610d78309e29b210ebca4f61a33097c03306
AntonKuksov <AntonKuksov@github.com>
2016-12-16 00:30:16 +0200

    All fixed. Formated.

--- CheckPlayerNum.java
+++ CheckPlayerNum.java
@@ -1,28 +1,25 @@
 package kodutoo;
 
-
-
 public class CheckPlayerNum {
-	
-	 int[] score = new int[2];
+
+	int[] score = new int[2];
 
 	Gener num1;
-	Gener2 num2;
-	
-	public CheckPlayerNum(Gener first, Gener2 second) {
+	Gener num2;
+
+	public CheckPlayerNum(Gener first, Gener second) {
 
 		num1 = first;
 		num2 = second;
 
 	}
-	
-	public String Kontrol() {
 
+	public String Kontrol() {
 
 		if (num1.sum < num2.sum) {
 			System.out.println("Second player wins!");
 			score[1]++;
-			String result = "Second player wins!"; /* + "GAME SCORE " + score[0] + ":" + score[1];*/
+			String result = RollDiceGUI.Second.player + " wins!";
 			KostiGUI.Resultlabel.setText(result);
 			System.out.println("GAME SCORE " + score[0] + ":" + score[1]);
 			return result;
@@ -31,34 +28,28 @@ public class CheckPlayerNum {
 		else if (num1.sum > num2.sum) {
 			System.out.println("First player wins!");
 			score[0]++;
-			String result = "First player wins!"; /* + "GAME SCORE " + score[0] + ":" + score[1];*/
+			String result = RollDiceGUI.First.player + " wins!";
 			KostiGUI.Resultlabel.setText(result);
 			System.out.println("GAME SCORE " + score[0] + ":" + score[1]);
 			return result;
 
 		} else {
-			
-			String result = "DRAW!"; /* + "GAME SCORE " + score[0] + ":" + score[1];*/
+
+			String result = "DRAW!";
 			KostiGUI.Resultlabel.setText(result);
 			System.out.println("DRAW!");
 			System.out.println("GAME SCORE " + score[0] + ":" + score[1]);
 			return result;
 
 		}
-			
-		
+
 	}
 
-	public String Score(){
-		
-		
+	public String Score() {
 
-			String arv = score[0] + ":" + score[1];
-			return arv;
-	
+		String arv = score[0] + ":" + score[1];
+		return arv;
+
 	}
-	
-		 
-	
 
 }
